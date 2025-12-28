a98fe6beaab120b982b9be3852ea376e943caec9
azraksk <yusraazrademirel@gmail.com>
2025-12-28 19:31:28 +0300

    Update project with latest changes

--- about.php
+++ about.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+
+$BASE_URL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http')
+          . '://' . $_SERVER['HTTP_HOST']
+          . '/blog-project';
+
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -45,7 +52,6 @@ if (isset($_POST['contact_submit'])) {
 ?>
 
 
-<?php include_once __DIR__ . "/includes/header.php"; ?>
 
 
 
--- backend/db.php
+++ backend/db.php
@@ -1,7 +1,7 @@
 <?php
 
 $host = "localhost";
-$dbname = "myblog_db";
+$dbname = "blog_proje";
 $username = "root";
 $password = "root";
 
